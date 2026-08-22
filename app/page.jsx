a428f7cca559d03198b63d355efc2c91e94fff3b
Yunana3053 <yunana3053@gmail.com>
2026-08-22 08:37:48 +0100

    Create Home component for Arewa Wake Up Movement

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{padding: "50px", textAlign: "center", fontFamily: "Arial"}}>
+      <h1>AREWA WAKE UP MOVEMENT</h1>
+      <p>
+        A nonpartisan, non-sectarian platform committed to peace, unity, 
+        education, and development in Northern Nigeria.
+      </p>
+      <p>We believe the challenges facing the North can only be solved by the people of the North.</p>
+    </main>
+  )
+}
